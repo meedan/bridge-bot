7183958cc5a4b001a112473b7d999aaa863ed6a3
Caio SBA <caiosba@gmail.com>
2017-05-02 13:57:05 -0300

    Ticket #5936: Making it more stable and sending log to CloudWatch

--- bot.js
+++ bot.js
@@ -3,49 +3,58 @@ var config = require('./config.js');
 var Lokka = require('lokka').Lokka;
 var Transport = require('lokka-transport-http').Transport;
 var header = require('basic-auth-header');
-
-var handleErrors = function(errors, data) {
-  var message = errors[0].message;
-  console.log(message);
-}
-
-var headers = {
-  'X-Check-Token': config.checkApi.apiKey
-};
-if (config.checkApi.httpAuth) {
-  var credentials = config.checkApi.httpAuth.split(':');
-  var basic = header(credentials[0], credentials[1]);
-  headers['Authorization'] = basic;
-}
-
-var client = new Lokka({ transport: new Transport(config.checkApi.url, { handleErrors, headers, credentials: false } ) });
-
-const mutationQuery = `($quote: String!, $pid: Int!, $annotation: String!) {
-  createProjectMedia: createProjectMedia(input: { clientMutationId: "1", project_id: $pid, quote: $quote, url: "", set_annotation: $annotation }) {
-    project_media {
-      dbid
-    }
-  }
-}`;
+var util = require('util');
 
 module.exports = botBuilder(function(request) {
-  const annotation = {
-    annotation_type: 'translation_request',
-    set_fields: JSON.stringify({
-      translation_request_type: 'viber',
-      translation_request_raw_data: JSON.stringify(request)
-    })
-  };
+  try {
+    var handleErrors = function(errors, data) {
+      var message = errors[0].message;
+      console.log('ERROR: ' + message);
+    };
+    
+    var headers = {
+      'X-Check-Token': config.checkApi.apiKey
+    };
 
-  const vars = {
-    quote: request.text,
-    pid: config.checkApi.projectId,
-    annotation: JSON.stringify(annotation)
-  };
-  
-  client.mutate(mutationQuery, vars).then(resp => {
-    console.log('ProjectMedia created with id ' + resp.createProjectMedia.project_media.dbid);
-  });
+    if (config.checkApi.httpAuth) {
+      var credentials = config.checkApi.httpAuth.split(':');
+      var basic = header(credentials[0], credentials[1]);
+      headers['Authorization'] = basic;
+    }
+    
+    const client = new Lokka({ transport: new Transport(config.checkApi.url, { handleErrors, headers, credentials: false } ) });
+    
+    const mutationQuery = `($quote: String!, $pid: Int!, $annotation: String!, $mid: String!) {
+      createProjectMedia: createProjectMedia(input: { clientMutationId: $mid, project_id: $pid, quote: $quote, url: "", set_annotation: $annotation }) {
+        project_media {
+          dbid
+        }
+      }
+    }`;
 
-  return 'Thank you. We have received your request for translation. You will receive a response shortly.';
+    const annotation = {
+      annotation_type: 'translation_request',
+      set_fields: JSON.stringify({
+        translation_request_type: 'viber',
+        translation_request_raw_data: JSON.stringify(request)
+      })
+    };
+
+    const vars = {
+      quote: request.text,
+      pid: config.checkApi.projectId,
+      annotation: JSON.stringify(annotation),
+      mid: parseInt(Math.random() * 10000, 10).toString()
+    };
+    
+    client.mutate(mutationQuery, vars).then(resp => {
+      console.log('DEBUG: Text "' + request.text + '" sent to the API, which replied with: ' + util.inspect(resp));
+    });
+
+    return 'Thank you. We have received your request for translation. You will receive a response shortly.';
+  }
+  catch (e) {
+    console.log('EXCEPTION: ' + e.message);
+    return 'Sorry, something went wrong. Please contact the support.';
+  }
 });
